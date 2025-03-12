7b42342c42698c77d600041b853ef2767fa1d067
anyubinn <anyubin0724@naver.com>
2025-03-12 11:16:48 +0900

    chore: Kiosk 주석 추가

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -9,6 +9,9 @@ import io.KioskInput;
 import menu.Menu;
 import menu.MenuItem;
 
+/**
+ * 키오스크와 관련된 기능을 처리하는 클래스
+ */
 public class Kiosk {
 
     private List<Menu> menus;
@@ -19,10 +22,14 @@ public class Kiosk {
         this.carts = new Cart();
     }
 
+    /**
+     * 키오스크의 전반적인 과정을 담당한다.
+     */
     public void start() {
 
         while (true){
             try {
+                // 메뉴 카테고리 선택
                 KioskOutput.printCategory(menus);
                 chooseCategory(menus);
             } catch (InputMismatchException | NumberFormatException e) {
@@ -31,7 +38,12 @@ public class Kiosk {
         }
     }
 
+    /**
+     * 사용자가 선택한 카테고리에 해당하는 메뉴를 출력하고, 메뉴 선택을 처리한다.
+     * @param menus 메뉴 리스트
+     */
     private void chooseCategory(List<Menu> menus) {
+        // 카테고리 번호 입력
         int num = KioskInput.inputNumber();
 
         if (num == 0) {
@@ -40,30 +52,41 @@ public class Kiosk {
         }
 
         if (num == 4) {
+            // 구매
             purchaseItem();
             return;
         }
 
         if (num == 5) {
+            // 장바구니가 비었는지 검사
             if (carts.isEmpty()) {
                 System.out.println("장바구니에 담긴 메뉴가 없습니다.");
                 return;
             }
+            // 장바구니 비우기
             System.out.println("장바구니에 담긴 메뉴를 전부 삭제합니다.");
             carts.clear();
             return;
         }
 
+        // 메뉴 번호와 다른 번호를 입력한 경우
         if (menus.size() < num) {
             System.out.println("없는 메뉴입니다. 다시 선택하세요.");
             return;
         }
+        // 선택한 카테고리에 해당하는 메뉴 출력
         Menu menu = menus.get(num - 1);
         KioskOutput.printMenu(menu.getCategory(), menu.getMenuItems());
+        // 메뉴 아이템 선택
         chooseMenu(menu.getMenuItems());
     }
 
+    /**
+     * 주문하고 싶은 메뉴 아이템을 선택한다.
+     * @param menuItems 주문하고 싶은 메뉴 아이템
+     */
     private void chooseMenu(List<MenuItem> menuItems) {
+        // 주문하고 싶은 메뉴 아이템 번호 입력
         int num = KioskInput.inputNumber();
 
         if (num == 0) {
@@ -71,16 +94,23 @@ public class Kiosk {
             return;
         }
 
+        // 메뉴 아이템 번호와 다른 번호를 입력한 경우
         if (menuItems.size() < num) {
             System.out.println("없는 메뉴입니다. 다시 선택하세요.");
             return;
         }
+        // 메뉴 아이템을 장바구니에 추가
         MenuItem menuItem = menuItems.get(num - 1);
         KioskOutput.printOption(menuItem.getName(), menuItem.getPrice());
         addCart(menuItem);
     }
 
+    /**
+     * 선택한 메뉴 아이템을 장바구니에 추가한다.
+     * @param menuItem 장바구니에 추가할 메뉴 아이템
+     */
     private void addCart(MenuItem menuItem) {
+        // 장바구니에 담을지 번호 입력
         int num = KioskInput.inputNumber();
 
         if (num == 2) {
@@ -88,42 +118,61 @@ public class Kiosk {
             return;
         }
 
+        // 선택사항에 없는 번호를 입력한 경우
         if (num != 1) {
             System.out.println("없는 메뉴입니다. 다시 선택하세요.");
             return;
         }
 
+        // 장바구니에 메뉴 아이템 추가
         carts.addItem(menuItem);
+        // 장바구니에 담은 메뉴 아이템 정보 출력
         KioskOutput.printOneCart(menuItem.getName());
     }
 
+    /**
+     * 장바구니에 담긴 메뉴 아이템을 결제한다.
+     */
     private void purchaseItem() {
+        // 장바구니에 담긴 메뉴 아이템의 총액을 계산
         int totalPrice = carts.getTotalPrice();
+        // 장바구니가 빈 경우
         if (carts.isEmpty()) {
             System.out.println("장바구니에 담긴 메뉴가 없습니다. 장바구니를 담아주세요.");
             return;
         }
+        // 장바구니에 담은 전체 메뉴 아이템 정보 출력
         KioskOutput.printAllCarts(carts, totalPrice);
+        // 결제, 메뉴판, 장바구니 제거 중 번호 입력
         int num = KioskInput.inputNumber();
 
         switch (num) {
             case 1 -> {
+                // 할인 정보 계산
                 calculateDiscountPrice(totalPrice);
+                // 장바구니 비우기
                 carts.clear();
             }
             case 2 -> {
                 System.out.println("메뉴판으로 돌아갑니다.");
             }
             case 3 -> {
+                // 장바구니에서 제거할 메뉴 아이템 선택
                 removeCartItem();
             }
+            // 선택사항에 없는 번호를 입력한 경우
             default -> {
                 System.out.println("없는 메뉴입니다. 다시 선택하세요.");
             }
         }
     }
 
+    /**
+     * 할인 유형을 선택하여 최종 결제 금액을 계산한다.
+     * @param totalPrice 할인 금액을 적용하기 전 총액
+     */
     private void calculateDiscountPrice(int totalPrice) {
+        // 할인 대상에 해당하는지 선택
         KioskOutput.printDiscountInfo();
         int num = KioskInput.inputNumber();
 
@@ -147,11 +196,17 @@ public class Kiosk {
             }
         }
 
+        // 할인 적용 후 최종 금액 출력
         KioskOutput.printTotalPrice(totalDiscountPrice);
     }
 
+    /**
+     * 입력한 메뉴 아이템을 장바구니에서 제거한다.
+     */
     private void removeCartItem() {
+        // 장바구니에서 제거할 메뉴 아이템 이름 입력
         String menuName = KioskInput.inputMenuName();
+        // 메뉴 아이템 제거
         carts.removeCartItem(menuName);
     }
 }
