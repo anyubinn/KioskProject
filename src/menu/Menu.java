37a220c6c7d0b8fddf63984e2742e674b6843c17
anyubinn <anyubin0724@naver.com>
2025-03-12 10:47:29 +0900

    chore: menu 주석 추가

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -2,6 +2,9 @@ package menu;
 
 import java.util.List;
 
+/**
+ * 메뉴 카테고리와 그에 속하는 메뉴 아이템을 관리하는 클래스이다.
+ */
 public class Menu {
     private String category;
     private List<MenuItem> menuItems;
@@ -11,10 +14,18 @@ public class Menu {
         this.menuItems = menuItems;
     }
 
+    /**
+     * 메뉴의 카테고리를 반환한다.
+     * @return 메뉴의 카테고리
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * 카테고리에 속하는 메뉴 아이템을 반환한다.
+     * @return 카테고리에 속하는 메뉴 아이템
+     */
     public List<MenuItem> getMenuItems() {
         return menuItems;
     }
--- src/menu/MenuItem.java
+++ src/menu/MenuItem.java
@@ -1,5 +1,8 @@
 package menu;
 
+/**
+ * 개별 메뉴 아이템의 정보를 관리하는 클래스이다.
+ */
 public class MenuItem {
     private String name;
     private int price;
@@ -11,14 +14,26 @@ public class MenuItem {
         this.comment = comment;
     }
 
+    /**
+     * 메뉴 아이템의 이름을 반환한다.
+     * @return 메뉴 아이템 이름
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * 메뉴 아이템의 가격을 반환한다.
+     * @return 메뉴 아이템 가격
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * 메뉴 아이템의 설명을 반환한다.
+     * @return 메뉴 아이템 설명
+     */
     public String getComment() {
         return comment;
     }
