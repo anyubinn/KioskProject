e45da79d3c471bd467252f522adf0bdad690361b
anyubinn <anyubin0724@naver.com>
2025-03-12 10:19:21 +0900

    chore: cart 주석 추가

--- src/cart/Cart.java
+++ src/cart/Cart.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 import menu.MenuItem;
 
+/**
+ * 장바구니와 관련된 처리를 하는 클래스이다.
+ */
 public class Cart {
     private Map<MenuItem, Integer> cartItems;
 
@@ -11,36 +14,63 @@ public class Cart {
         cartItems = new HashMap<>();
     }
 
+    /**
+     * 장바구니에 메뉴 아이템을 추가한다.
+     * @param item 장바구니에 추가할 메뉴 아이템
+     */
     public void addItem(MenuItem item) {
+        // 이미 담은 메뉴라면 개수 증가, 처음 담는 메뉴라면 새로 추가
         cartItems.put(item, cartItems.getOrDefault(item, 0) + 1);
     }
 
+    /**
+     * 장바구니에 담은 메뉴 아이템과 개수를 반환한다.
+     * @return 장바구니에 담은 메뉴 아이템과 개수
+     */
     public Map<MenuItem, Integer> getCartItems() {
         return cartItems;
     }
 
+    /**
+     * 장바구니가 비어있는지 여부를 반환한다.
+     * @return 장바구니가 비어있다면 true, 아니면 false
+     */
     public boolean isEmpty() {
         return cartItems.isEmpty();
     }
 
+    /**
+     * 장바구니를 전부 비운다.
+     */
     public void clear() {
         cartItems.clear();
     }
 
+    /**
+     * 장바구니에 담긴 총액을 반환한다.
+     * @return 장바구니에 담은 메뉴의 총액
+     */
     public int getTotalPrice() {
         return cartItems.entrySet().stream()
                 .mapToInt(cart -> cart.getKey().getPrice() * cart.getValue())
                 .sum();
     }
 
+    /**
+     * 특정 하나의 메뉴 아이템을 장바구니에서 제거한다.
+     * @param menuName 장바구니에서 제거할 메뉴 이름
+     */
     public void removeCartItem(String menuName) {
+        // 메뉴 이름이 장바구니에 존재하는지 검사
         boolean isMenuNameExist = cartItems.entrySet().stream()
                 .anyMatch(cart -> cart.getKey().getName().equals(menuName));
 
+        // 장바구니에 없다면
         if (!isMenuNameExist) {
             System.out.println(menuName + "가 장바구니에 존재하지 않습니다.");
             return;
         }
+        // 장바구니에 존재한다면
         cartItems.entrySet().removeIf(cart -> cart.getKey().getName().equals(menuName));
         System.out.println(menuName + "를 장바구니에서 제거했습니다.");
     }
--- src/cart/Discount.java
+++ src/cart/Discount.java
@@ -1,5 +1,8 @@
 package cart;
 
+/**
+ * 할인 정보를 관리하는 enum 클래스
+ */
 public enum Discount {
     PATRIOT("국가유공자", 0.1),
     SOLDIER("군인", 0.05),
@@ -14,14 +17,27 @@ public enum Discount {
         this.discountRate = discountRate;
     }
 
+    /**
+     * 할인 유형을 반환한다.
+     * @return 할인 유형
+     */
     public String getDiscountType() {
         return discountType;
     }
 
+    /**
+     * 할인율을 반환한다.
+     * @return 할인율
+     */
     public double getDiscountRate() {
         return discountRate;
     }
 
+    /**
+     * 할인 적용 후 최종 금액을 반환한다.
+     * @param totalPrice 할인 적용 전 총액
+     * @return 할인 적용 후 최종 금액
+     */
     public int getTotalDiscountPrice(int totalPrice) {
         return (int) (totalPrice - (totalPrice * this.discountRate));
     }
